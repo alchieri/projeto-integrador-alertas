d7e385e9a5c85e469ebf50a07a12b04b53668e5f
altrsl <alt_rsl@hotmail.com>
2023-11-21 20:52:38 -0300

    css do registro de usuário

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,14 +39,38 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Link Imagem perfil -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Link imagem de perfil')" />
+
+            <x-text-input class="block mt-1 w-full" type="text"/>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ml-4">
+            <button id="botao" class="ml-4">
                 {{ __('Register') }}
-            </x-primary-button>
+            </button>
         </div>
+        
+        <style>
+             #botao:hover {
+                    background-color: #201f29;
+            }
+
+            #botao {
+                background-color: #3f3f43;
+                color: white;
+                padding: 10px 20px;
+                border: none;
+                cursor: pointer;
+                border-radius: 10px;
+
+            }
+        </style>
+
     </form>
 </x-guest-layout>
